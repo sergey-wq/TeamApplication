72e23ecba4503406da66ad2bc63f7aad200d645b
sergeykin <sergeykin1997@mail.ru>
2022-03-26 19:28:47 +0300

    feat: added app

---
+++ HealthyApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthyApp
+//
+//  Created by Sergey Runovich on 26.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
